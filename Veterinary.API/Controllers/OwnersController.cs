239734f59208017d7dce65cabcea8058e66cc4b2
cristian0223 <cc7847443@gmail.com>
2023-08-31 20:25:34 -0500

    Version 0 - Commit Main

---
+++ Veterinary.API/Controllers/OwnersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Veterinary.API.Data;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Controllers
+{
+    [ApiController]
+    [Route("api/owners")]    
+    public class OwnersController:ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public OwnersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //Get por parametro
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+
+            //200 Ok
+            //204, etc
+
+            //busca el primero que encuentre
+            await _context.Owners.FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok();
+        }
+
+        //Insertar un registro
+        [HttpPost]
+        public async Task<ActionResult> Post(Owner owner)
+        {
+
+            _context.Add(owner);
+            await _context.SaveChangesAsync();
+
+            return Ok(owner);
+        }
+
+    }
+}
---
+++ Veterinary.Shared/Entities/Owner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinary.Shared.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El documento es requerido")]
+        public string Documento { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string LastName { get; set; } = string.Empty;
+        public string FixedPhone {  get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El numero celular es requerido")]
+        public string CellPhone {  get; set; } = string.Empty;
+
+        public string Adress {  get; set; } = string.Empty;
+        public string FullName => $"{FirstName}{LastName}";
+
+    }
+}
